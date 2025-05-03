d046415981388d3a8c6af57e275599e8f6813136
Jennifer Traeger <jgoodnight7272@gmail.com>
2025-05-02 21:24:17 -0700

    resolve error and run java 21

--- my_backend/src/main/java/com/example/demo/WebConfig.java
+++ my_backend/src/main/java/com/example/demo/WebConfig.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000");
     }
