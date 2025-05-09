cf1252ba0045041ca060fe665b1beaadd11f7f1e
Jennifer Traeger <jgoodnight7272@gmail.com>
2025-05-09 12:13:06 -0700

    New header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8080/app/hello')
+    fetch('http://localhost:8080/app/home')
     .then(res => res.text())
     .then(data => setMessage(data))
     .catch((err) => console.error(err));;
@@ -16,19 +16,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Widget Board</h1>
       </header>
-      <h2>React + Spring Boot</h2>
       <p>{message}</p>
     </div>
   );
