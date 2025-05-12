f3999d5553279c859783e2cf6a25671e1afe1d66
Jennifer Traeger <jgoodnight7272@gmail.com>
2025-05-12 16:53:00 -0700

    Day, Time and Icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
+import sun from './sun.svg';
+import moon from './moon.png';
 import './App.css';
 import {useEffect, useState} from 'react';
+import Clock from 'react-live-clock';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -15,8 +18,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="ogo" />
         <h1>Widget Board</h1>
+        <span className="Date-time">
+          <Clock className="Date-clock" format={'dddd, MMMM Do YYYY'} timezone={'US/Pacific'}/>
+          <span className="Time">
+            <span className="AM-PM-logos">
+              <img src={sun} className="AM-sun" alt="amSun"/>
+              <img src={moon} className="PM-moon" alt="pmMoon"/>
+            </span>
+            <Clock className="Time-clock" format={'h:mm:ss'} ticking={true} timezone={'US/Pacific'}/>
+            <span>PST</span>
+          </span>
+        </span>
       </header>
       <p>{message}</p>
     </div>
