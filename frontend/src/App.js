7db821009286e35527b8f117706c6d1f52215a62
Jennifer Traeger <jgoodnight7272@gmail.com>
2025-05-19 16:29:52 -0700

    new color scheme and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logos/logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
 import Clock from './components/Clock';
+import gitIcon from './icons/git.png';
+import linkedinIcon from './icons/linkedin.png';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -20,7 +22,36 @@ function App() {
         <h1>Widget Board</h1>
         <Clock/>
       </header>
-      <p>{message}</p>
+      <main className="app-main">
+        <p>{message}</p>
+      </main>
+      <footer className="app-footer">
+        <span className="footer-about">
+          <h4>About The Developer</h4>
+          <p>
+            Passionate about building great software with great people! 
+            To me, meaningful work comes from impactful solutions, collaboration and team synergy. 
+            As a Software Developer at Opus, I work remotely with a nationwide team, delivering scalable, high-quality software. 
+            Always looking to grow, refine my skills, and contribute to projects that make a difference.
+          </p>
+        </span>
+        <span className="footer-contact-info"> 
+          <h4>Contact Info</h4>
+          <p>
+            (509)690-7476
+            <br/>
+            jgoodnight7272@gmail.com
+          </p>
+          <span className="footer-contact-icons">
+            <a href="https://www.linkedin.com/in/jtraeger/" target="_blank" rel="noreferrer">
+              <img src={linkedinIcon} alt="Linkedin Icon" className="linkedin-icon"></img>
+            </a>
+            <a href="https://github.com/jgoodnight72" target="_blank" rel="noreferrer">
+              <img src={gitIcon} alt="Git Icon" className="git-icon"></img>
+            </a>
+          </span>
+        </span>
+      </footer>
     </div>
   );
 }
